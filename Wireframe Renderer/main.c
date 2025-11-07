26f34a1d70136da7bb75559c56b100f86a48e744
MonsieurSingh <teghjyot@icloud.com>
2025-11-08 00:50:58 +0100

    Initial Commit

---
+++ Wireframe Renderer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Wireframe Renderer
+//
+//  Created by Teghjyot Singh on 8/11/2025.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return EXIT_SUCCESS;
+}
