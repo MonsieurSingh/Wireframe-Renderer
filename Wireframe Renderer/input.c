172b8732d784c3ecdcf261ead0019291e77ae6e5
MonsieurSingh <teghjyot@icloud.com>
2025-11-13 00:00:03 +0100

    pointer error

--- Wireframe Renderer/input.c
+++ Wireframe Renderer/input.c
@@ -9,65 +9,60 @@
 
 int key_handler(int keycode, void *param)
 {
-	t_data	*d;
-	t_model		model;
-	t_camera	cam;
+	t_data *d = (t_data *)param;
 
-	d = (t_data *)param;
-	model = d->state.model;
-	cam = d->state.camera;
-	if (keycode == kVK_ANSI_W) model.rotate.x -= 0.05f;
-	if (keycode == kVK_ANSI_S) model.rotate.x += 0.05f;
-	if (keycode == kVK_ANSI_A) model.rotate.y -= 0.05f;
-	if (keycode == kVK_ANSI_D) model.rotate.y += 0.05f;
-	if (keycode == kVK_ANSI_Q) model.rotate.z -= 0.05f;
-	if (keycode == kVK_ANSI_E) model.rotate.z += 0.05f;
-
-	if (keycode == kVK_ANSI_1) cam.target.x -= 0.05f;
-	if (keycode == kVK_ANSI_2) cam.target.x += 0.05f;
-	if (keycode == kVK_ANSI_3) cam.target.y -= 0.05f;
-	if (keycode == kVK_ANSI_4) cam.target.y += 0.05f;
-	if (keycode == kVK_ANSI_5) cam.target.z -= 0.05f;
-	if (keycode == kVK_ANSI_6) cam.target.z += 0.05f;
+	if (keycode == kVK_ANSI_W) d->state.model.rotate.x -= 0.05f;
+	if (keycode == kVK_ANSI_S) d->state.model.rotate.x += 0.05f;
+	if (keycode == kVK_ANSI_A) d->state.model.rotate.y -= 0.05f;
+	if (keycode == kVK_ANSI_D) d->state.model.rotate.y += 0.05f;
+	if (keycode == kVK_ANSI_Q) d->state.model.rotate.z -= 0.05f;
+	if (keycode == kVK_ANSI_E) d->state.model.rotate.z += 0.05f;
 	
-	if (keycode == kVK_ANSI_Z) cam.eye.x -= 0.05f;
-	if (keycode == kVK_ANSI_X) cam.eye.x += 0.05f;
-	if (keycode == kVK_ANSI_C) cam.eye.y -= 0.05f;
-	if (keycode == kVK_ANSI_V) cam.eye.y += 0.05f;
-	if (keycode == kVK_ANSI_B) cam.eye.z -= 0.05f;
-	if (keycode == kVK_ANSI_N) cam.eye.z += 0.05f;
+	if (keycode == kVK_ANSI_1) d->state.camera.target.x -= 0.05f;
+	if (keycode == kVK_ANSI_2) d->state.camera.target.x += 0.05f;
+	if (keycode == kVK_ANSI_3) d->state.camera.target.y -= 0.05f;
+	if (keycode == kVK_ANSI_4) d->state.camera.target.y += 0.05f;
+	if (keycode == kVK_ANSI_5) d->state.camera.target.z -= 0.05f;
+	if (keycode == kVK_ANSI_6) d->state.camera.target.z += 0.05f;
+	
+	if (keycode == kVK_ANSI_Z) d->state.camera.eye.x -= 0.05f;
+	if (keycode == kVK_ANSI_X) d->state.camera.eye.x += 0.05f;
+	if (keycode == kVK_ANSI_C) d->state.camera.eye.y -= 0.05f;
+	if (keycode == kVK_ANSI_V) d->state.camera.eye.y += 0.05f;
+	if (keycode == kVK_ANSI_B) d->state.camera.eye.z -= 0.05f;
+	if (keycode == kVK_ANSI_N) d->state.camera.eye.z += 0.05f;
+	
+	if (keycode == kVK_LeftArrow) d->state.model.translate.x -= 5.00f;
+	if (keycode == kVK_RightArrow) d->state.model.translate.x += 5.00f;
+	if (keycode == kVK_UpArrow) d->state.model.translate.y -= 5.00f;
+	if (keycode == kVK_DownArrow) d->state.model.translate.y += 5.00f;
+	if (keycode == kVK_ANSI_Period) d->state.model.translate.z -= 5.00f;
+	if (keycode == kVK_ANSI_Comma) d->state.model.translate.z += 5.00f;
+	
+	if (keycode == kVK_ANSI_1) d->state.model.scale.y -= 0.05f;
+	if (keycode == kVK_ANSI_2) d->state.model.scale.y += 0.05f;
+	if (keycode == kVK_ANSI_3) d->state.model.scale.x -= 0.05f;
+	if (keycode == kVK_ANSI_4) d->state.model.scale.x += 0.05f;
+	if (keycode == kVK_ANSI_Minus) d->state.model.scale.z -= 0.05f;
+	if (keycode == kVK_ANSI_Equal) d->state.model.scale.z += 0.05f;
 
-	if (keycode == kVK_LeftArrow) model.translate.x -= 5.00f;
-	if (keycode == kVK_RightArrow) model.translate.x += 5.00f;
-	if (keycode == kVK_UpArrow) model.translate.y -= 5.00f;
-	if (keycode == kVK_DownArrow) model.translate.y += 5.00f;
-	if (keycode == kVK_ANSI_Period) model.translate.z -= 5.00f;
-	if (keycode == kVK_ANSI_Comma) model.translate.z += 5.00f;
-	if (keycode == kVK_ANSI_1) model.scale.y -= 0.05f;
-	if (keycode == kVK_ANSI_2) model.scale.y += 0.05f;
-	if (keycode == kVK_ANSI_3) model.scale.x -= 0.05f;
-	if (keycode == kVK_ANSI_4) model.scale.x += 0.05f;
-	if (keycode == kVK_ANSI_Minus) model.scale.z -= 0.05f;
-	if (keycode == kVK_ANSI_Equal) model.scale.z += 0.05f;
 	return 0;
 }
 
 int mouse_handler(int button, int x, int y, void *param)
 {
 	t_data	*d;
-	t_model	model;
 
 	d = (t_data *)param;
-	model = d->state.model;
 	if (button == 4)
 	{
-		model.scale.x += 0.05;
-		model.scale.y += 0.05;
+		d->state.model.scale.x += 0.05;
+		d->state.model.scale.y += 0.05;
 	}
 	else if (button == 5)
 	{
-		model.scale.x -= 0.05;
-		model.scale.y -= 0.05;
+		d->state.model.scale.x -= 0.05;
+		d->state.model.scale.y -= 0.05;
 	}
 	return 0;
 }
